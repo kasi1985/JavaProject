c2a9c81815fd6f46079cef5e567c4b1f235ef5e4
kasi1985 <kasi.vundavalli@gmail.com>
2022-07-25 20:46:17 +0530

    first commit for intial java project

---
+++ src/main/java/com/client/ProjectGitApplication.java
@@ -0,0 +1,23 @@
+package com.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/hello")
+public class ProjectGitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectGitApplication.class, args);
+	}
+	
+	@GetMapping
+	public String hello() {
+		return "Hello..... Kasi!!";
+	}
+
+}
